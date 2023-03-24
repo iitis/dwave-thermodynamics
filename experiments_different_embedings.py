aac8b238990b654f36d18975df2bf8a9ea0b9139
tomsmierz <smierzchalski.tomek@gmail.com>
2023-03-24 22:51:58 +0100

    results Q2

--- experiments_different_embedings.py
+++ experiments_different_embedings.py
@@ -88,7 +88,7 @@ def energy(s: np.ndarray, h: np.ndarray, J: np.ndarray): return np.dot(np.dot(s,
 if __name__ == "__main__":
 
     # Setup
-    qpu_sampler = DWaveSampler(solver='DW_2000Q_6')
+    qpu_sampler = DWaveSampler(solver='DW_2000Q_6', token="DEV-ea8fe294d81c576d653e3e925574c4f9b9fa13dc")
     target = qpu_sampler.to_networkx_graph()
     middle_label = 1023
 
@@ -129,8 +129,8 @@ if __name__ == "__main__":
     anneal_param = 0.41
     beta = 1
     gibbs_num_steps = 10 ** 4
-    quadrant = Q1
-    quadrant_name = "Q1"
+    quadrant = Q2
+    quadrant_name = "Q2"
 
     mean_E_pause = {}
     var_E_pause = {}
