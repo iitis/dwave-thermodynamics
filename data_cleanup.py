25770e7d3bda032a8c94da55079282cb9857a88c
tomsmierz <smirzchalski.tomek@gmail.com>
2023-04-25 13:55:46 +0200

    added clean_pausing_with_h

--- data_cleanup.py
+++ data_cleanup.py
@@ -1,6 +1,20 @@
 import pandas as pd
-import pickle
 import os
+import numpy as np
 
 cwd = os.getcwd()
 
+
+def clean_pausing_with_h():
+    new_df = pd.DataFrame()
+    for h in np.linspace(0.1, 1.0, num=10):
+        h = round(h, 1)
+        df = pd.read_csv(os.path.join(cwd, f"results/raw_data/pausing__beta_1_h{h}_s0.41.csv"),
+                         sep=";")
+        df["h"] = [h for _ in df.iterrows()]
+        new_df = pd.concat([new_df, df], ignore_index=True)
+    new_df.to_csv(os.path.join(cwd, f"results/raw_data/pausing_with_h_beta_1_s_0.41.csv"), sep=";", index=False)
+
+
+if __name__ == "__main__":
+    clean_pausing_with_h()
