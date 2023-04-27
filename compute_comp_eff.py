8524bffdbdf56ca3ad983f1098db9f0c6ca5bc1a
tomsmierz <smirzchalski.tomek@gmail.com>
2023-04-27 13:55:40 +0200

    computed measures of computational eff. result + code

---
+++ compute_comp_eff.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import os
+from tqdm import tqdm
+import pickle
+
+cwd = os.getcwd()
+
+prob = {}
+distance = {}
+Q = 4
+way = "reverse"
+data = f"Q{Q}_{way}"
+
+df = pd.read_csv(os.path.join(cwd, "results", "raw_data", f"{data}.csv"), sep=";")
+df.drop(["Unnamed: 0"], axis=1, inplace=True)
+
+
+if __name__ == "__main__":
+
+    for tau in df.annealing_time.unique().tolist():
+        p = 0
+        c = 0
+        s = 0
+        count = 0
+        #  h = 1  & (df["h"] == h)  _h_{h} <- stuff for computing with h
+        for init_state in tqdm(df[(df["annealing_time"] == tau)].init_state.unique().tolist(), desc=f"{tau} "):
+
+            temp_df = df[(df["annealing_time"] == tau) & (df["init_state"] == init_state)]
+            c += 1
+            if -299 in temp_df["energy"].values:
+                p += 1
+            s += sum([(row[1]["energy"]/-299) * row[1]["num_occurrences"] for row in temp_df.iterrows()])/len(temp_df)
+
+        print(f"probability for tau {tau}: ", p/c)
+        print(f"closeness for tau {tau}: ", s/c)
+        prob[tau] = p/c
+        distance[tau] = s/c
+
+    with open(os.path.join(cwd, "results", f"results_eff_{data}.pkl"), "wb") as f:
+        l = [prob, distance]
+        pickle.dump(l, f)
---
+++ draw_plots.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import pickle
+import numpy as np
+import os
+
+cwd = os.getcwd()
+
+beta = 1
+dataset = f"results_eff_pausing_beta_{beta}_s_0.5"
+
+if __name__ == "__main__":
+    with open(os.path.join(cwd, "results", f"{dataset}.pkl"), "rb") as f:
+        prob, dist = pickle.load(f)
+
+    fig, ax1 = plt.subplots()
+    title = f"Reverse annealing with pausing and without bias (h) \n for beta = {beta}"
+    plt.title(title)
+
+    color1 = "tab:blue"
+    ax1.set_xlabel("Annealing time ($\\mu s$)")
+    ax1.set_ylabel("Success probability")
+    ax1.plot(list(prob.keys()), list(prob.values()), color=color1)
+    ax1.tick_params(axis="y", labelcolor=color1)
+
+    ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+    color2 = "tab:red"
+    ax2.set_ylabel("Average distance from the ground state")
+    ax2.plot(list(dist.keys()), [v for v in dist.values()], color=color2)
+    ax2.tick_params(axis="y", labelcolor=color2)
+
+    plt.show()
+    fig.savefig(os.path.join(cwd, "results", "plots", f"{dataset}.png"))
