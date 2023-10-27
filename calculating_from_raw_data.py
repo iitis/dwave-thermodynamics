75856dbd0e5e6b1f6fd39cf4f6d55e9f8dcd6b14
tomsmierz <smirzchalski.tomek@gmail.com>
2023-10-27 12:37:52 +0200

    WIP

--- calculating_from_raw_data.py
+++ calculating_from_raw_data.py
@@ -4,79 +4,58 @@ import pandas as pd
 import os
 from scipy import optimize
 from tqdm import tqdm
+from utils import energy, vectorize, gibbs_sampling_ising, pseudo_likelihood
 
-
-def pseudo_likelihood(beta_eff, samples):
-    J = - 1.0
-    L = 0.0
-    N = samples.shape[1]
-    D = samples.shape[0]
-    for i in tqdm(range(D-1), desc="i"):
-        for j in range(N-1):
-            if j == 0:
-                L += -np.log(1+np.exp(-2*(J*samples[i,j]*samples[i,j+1])*beta_eff))
-            elif j == N-1:
-                L += -np.log(1+np.exp(-2*(J*samples[i,j]*samples[i,j-1])*beta_eff))
-            else:
-                L += -np.log(1+np.exp(-2*(J*samples[i,j]*samples[i,j+1]+J*samples[i,j]*samples[i,j-1])*beta_eff))
-    return -L/(N*D)
-
+rng = np.random.default_rng()
 cwd = os.getcwd()
 
 
-def vectorize(h: dict, J: dict):
-    # We assume that h an J are sorted
-    h_vect = np.array(list(h.values()))
-    n = len(h_vect)
-    J_vect = np.zeros((n, n))
-    for key, value in J.items():
-        J_vect[key[0]][key[1]] = value
-    return h_vect, J_vect
-
-
-with open("C:\\Users\\tsmierzchalski\\PycharmProjects\\dwave-thermodynamics\\results\\average_spin_energy_s0.5_beta0.1.pkl",
-          "rb") as f:
-    mean_E, var_E = pickle.load(f)
-    mean_E = [e/2 for e in mean_E]
-
-mean_Q = []
-var_Q = []
-beta_eff = []
-chain_length = 300
-h = {i: 0 for i in range(chain_length)}
-J = {(i, i + 1): 1.0 for i in range(chain_length - 1)}
-h_vect, J_vect = vectorize(h, J)
-
-for t in [1.0, 23.11, 45.22, 67.33, 89.44, 111.55, 133.66, 155.77, 177.88, 200.0]:
+def calculate_from_raw_data(h_vect, J_vect, chain_length, df: pd.DataFrame):
+    init_states = df.init_state.unique().tolist()
     E_fin = []
     configurations = []
     Q = []
-    df = pd.read_csv(os.path.join(cwd, f"results\\raw_data\\raw_data_reverse_tau_{t}_beta_0.1_s0.5.csv"),
-                     sep=";", index_col=0)
-    for init in tqdm(df.init_state.unique().tolist(), desc=f"{t}"):
-        samples = df[df.init_state == init]
-
-        initial_state = eval(init)
-        init_state = np.array(list(initial_state.values()))
-        E_init = (np.dot(init_state, np.dot(J_vect, init_state)))/chain_length
-
-        for sample in samples.itertuples(index=False):
-            s = eval(sample.sample)
+    for state in init_states:
+        temp_df = df[df["init_state"] == state]
+        state = eval(state)
+        init_state = np.array(list(state.values()))
+        E_init = energy(init_state, h_vect, J_vect) / chain_length
+        samples = temp_df["sample"].tolist()
+        for s in samples:
+            s = eval(s)
             final_state = np.array(list(s.values()))
-            assert s == {i: final_state[i] for i in range(300)}  # sanity check
-            E_fin.append(sample.energy/chain_length)
+            E_fin.append(energy(final_state, h_vect, J_vect) / chain_length)
             configurations.append(final_state)
-            Q.append(sample.energy/chain_length - E_init)
-
-    optim = optimize.minimize(pseudo_likelihood, 1.0, args=(np.array(configurations),))
-    beta_eff.append(optim.x)
-    mean_Q.append(np.mean(np.array(Q)))
-    var_Q.append(np.var(np.array(Q)))
-    with open("C:\\Users\\tsmierzchalski\\PycharmProjects\\dwave-thermodynamics\\results\\checkpoints\\"
-              "checkpoint_result_reverse_s0.5_beta0.1_200.pkl", "wb") as f:
-        pickle.dump([beta_eff, mean_E, var_E, mean_Q, var_Q], f)
+            Q.append(energy(final_state, h_vect, J_vect) / chain_length - E_init)
+    return E_fin, configurations, Q
 
 
-with open("C:\\Users\\tsmierzchalski\\PycharmProjects\\dwave-thermodynamics\\results\\results_reverse_s0.5_beta0.1.pkl",
-          "rb") as f:
-    pickle.dump([beta_eff, mean_E, var_E, mean_Q, var_Q], f)
+if __name__ == '__main__':
+    with open(os.path.join(cwd, "paper2", "instance.pkl"), "rb") as f:
+        h, J = pickle.load(f)
+    h_vect, J_vect = vectorize(h, J)
+    chain_length = 300
+    mean_E = {}
+    var_E = {}
+    mean_Q = {}
+    var_Q = {}
+    beta_eff = {}
+    for anneal_param in tqdm(np.linspace(0, 1, num=25), desc="Anneal parameters"):
+        if anneal_param < 0.38 or anneal_param > 0.74:
+            continue
+        s = f"{anneal_param:.2f}"
+
+        path = os.path.join(cwd, "results", "raw_data", "pegasus_thermo", f"raw_data_pegasus_thermo_0_{s}.csv")
+        raw_data = pd.read_csv(path, sep=";", index_col=0)
+
+        E_fin, configurations, Q = calculate_from_raw_data(h_vect, J_vect, chain_length, raw_data)
+
+        optim = optimize.minimize(pseudo_likelihood, 1.0, args=(np.array(configurations),))
+        beta_eff[(0, anneal_param)] = optim.x
+        mean_E[(0, anneal_param)] = np.mean(np.array(E_fin))
+        var_E[(0, anneal_param)] = np.var(np.array(E_fin))
+        mean_Q[(0, anneal_param)] = np.mean(np.array(Q))
+        var_Q[(0, anneal_param)] = np.var(np.array(Q))
+
+        with open(os.path.join(cwd, "results\\checkpoints", f"checkpoints_pegasus_thermo_0_rest.pkl"), "wb") as f:
+            pickle.dump([beta_eff, mean_E, var_E, mean_Q, var_Q], f)
--- read_data.py
+++ read_data.py
@@ -10,32 +10,64 @@ with open(os.path.join(cwd, "results", "checkpoints", "checkpoints_pegasus_therm
 with open(os.path.join(cwd, "results", "checkpoints", "checkpoints_pegasus_thermo.pkl"), "rb") as f:
     beta_eff_0, mean_E_0, var_E_0, mean_Q_0, var_Q_0 = pickle.load(f)
 
+with open(os.path.join(cwd, "results", "checkpoints", "checkpoints_pegasus_thermo_0+++.pkl"), "rb") as f:
+    beta_eff_0_2, mean_E_0_2, var_E_0_2, mean_Q_0_2, var_Q_0_2 = pickle.load(f)
+
+with open(os.path.join(cwd, "results", "checkpoints", "checkpoints_pegasus_thermo_0_rest.pkl"), "rb") as f:
+    beta_eff_0_3, mean_E_0_3, var_E_0_3, mean_Q_0_3, var_Q_0_3 = pickle.load(f)
+
+with open(os.path.join(cwd, "results", "checkpoints", "checkpoints_pegasus_thermo_60.pkl"), "rb") as f:
+    beta_eff_60, mean_E_60, var_E_60, mean_Q_60, var_Q_60 = pickle.load(f)
+
+with open(os.path.join(cwd, "results", "checkpoints", "checkpoints_pegasus_thermo_60+.pkl"), "rb") as f:
+    beta_eff_60_2, mean_E_60_2, var_E_60_2, mean_Q_60_2, var_Q_60_2 = pickle.load(f)
+
 with open(os.path.join(cwd, "results", "checkpoints", "checkpoints_pegasus_thermo_80.pkl"), "rb") as f:
     beta_eff_80, mean_E_80, var_E_80, mean_Q_80, var_Q_80 = pickle.load(f)
 
 with open(os.path.join(cwd, "results", "checkpoints", "checkpoints_pegasus_thermo_100.pkl"), "rb") as f:
     beta_eff_100, mean_E_100, var_E_100, mean_Q_100, var_Q_100 = pickle.load(f)
 
-
 beta_eff = copy.deepcopy(beta_eff_0)
+beta_eff.update(beta_eff_0_2)
+beta_eff.update(beta_eff_0_3)
 beta_eff.update(beta_eff_20_60)
+beta_eff.update(beta_eff_60)
+beta_eff.update(beta_eff_60_2)
 beta_eff.update(beta_eff_100)
 
 mean_E = copy.deepcopy(mean_E_0)
+mean_E.update(mean_E_0_2)
+mean_E.update(mean_E_0_3)
 mean_E.update(mean_E_20_60)
+mean_E.update(mean_E_60)
+mean_E.update(mean_E_60_2)
 mean_E.update(mean_E_100)
 
 var_E = copy.deepcopy(var_E_0)
+var_E.update(var_E_0_2)
+var_E.update(var_E_0_3)
 var_E.update(var_E_20_60)
+var_E.update(var_E_60)
+var_E.update(var_E_60_2)
 var_E.update(var_E_100)
 
 mean_Q = copy.deepcopy(mean_Q_0)
+mean_Q.update(mean_Q_0_2)
+mean_Q.update(mean_Q_0_3)
 mean_Q.update(mean_Q_20_60)
+mean_Q.update(mean_Q_60)
+mean_Q.update(mean_Q_60_2)
 mean_Q.update(mean_Q_100)
 
 var_Q = copy.deepcopy(var_Q_0)
+var_Q.update(var_Q_0_2)
+var_Q.update(var_Q_0_3)
 var_Q.update(var_Q_20_60)
+var_Q.update(var_Q_60)
+var_Q.update(var_Q_60_2)
 var_Q.update(var_Q_100)
 
-with open(os.path.join(cwd, "results", "pegasus_thermo_almost_all.pkl"), "wb") as f:
+print(list(beta_eff.keys()))
+with open(os.path.join(cwd, "results", "pegasus_thermo_all.pkl"), "wb") as f:
     pickle.dump([beta_eff, mean_E, var_E, mean_Q, var_Q], f)
